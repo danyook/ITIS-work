24544bdbba16861e7fdf9d31f90334ddc38e2d87
Daniil <tdm290705@gmail.com>
2024-05-22 17:30:30 +0300

    семестр 2 контрольная 2

--- src/main/java/ru/itis/inf304/progEnikeyAuditor/sem2_KR2/FileContent.java
+++ src/main/java/ru/itis/inf304/progEnikeyAuditor/sem2_KR2/FileContent.java
@@ -1,8 +1,8 @@
 package ru.itis.inf304.progEnikeyAuditor.sem2_KR2;
 
 public class FileContent {
-    int k; // размер в байтах строки с данными в формате UTF-8
-    String s; //строка с данными
-    int d; //контрольное число - количество символов в строке с данными
-    int p; //номер части
+    public int k; // размер в байтах строки с данными в формате UTF-8
+    public String s; //строка с данными
+    public int d; //контрольное число - количество символов в строке с данными
+    public int p; //номер части
 }
